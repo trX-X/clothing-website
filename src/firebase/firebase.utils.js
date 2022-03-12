498ff09bbbf192d2e7bb11df99b8aa2efe04f289
trX-X <tarinkumarpodavu4@gmail.com>
2022-03-12 23:39:16 +0530

    Implemented Sign Up with email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,15 +10,40 @@ const config = {
   messagingSenderId: '573497696623',
   appId: '1:573497696623:web:0531cb8f6788f11781089a',
 };
-
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date()
+    try{
+      await userRef.set({
+        displayName, 
+        email,
+        createdAt,
+        ...additionalData
+      })
+    }catch(error){
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account'})
+provider.setCustomParameters({ prompt: 'select_account' });
 
-export const signInWithGoogle = () => auth.signInWithPopup(provider).catch(() => {})
+export const signInWithGoogle = () =>
+  auth.signInWithPopup(provider).catch(() => {});
 
 export default firebase;
